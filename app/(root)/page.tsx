daa887846efa99744681647b7dbda4c20b064b53
hazhir <hazhir6690@gmail.com>
2024-05-25 11:47:18 +0330

    feature: Using chart js to display the user panel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+
+ const loggedIn = { firstName: "hazhir" };
  return (
-  <div>
-   Home
-  </div>
+  <section className="home">
+   <div className="home-content">
+    <header className="home-header">
+     <HeaderBox
+      type="greeting"
+      title="Welcome"
+      user={loggedIn?.firstName || "Guest"}
+      subtext="Access and manage your account and transaction efficiently."
+     />
+
+     <TotalBalanceBox
+      accounts={[]}
+      totalBanks={1}
+      totalCurrentBalance={1250.35}
+     />
+    </header>
+   </div>
+  </section>
  );
 };
 
