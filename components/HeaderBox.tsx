4e9f09b9cf856d61f1278fcb681d7f442a9f1bde
hazhir <hazhir6690@gmail.com>
2024-06-05 17:33:27 +0330

    feat:implementation function for create public token exchange

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeaderBox = ({ type = "title", title, subtext, user }: HeaderBoxProps) => {
  return (
   <div className="header-box">
-   <h1 className="">
+   <h1 className="header-box-title">
     {title}
     {type==='greeting'&&(
      <span className="text-bankGradient">&nbsp;{user}</span>
